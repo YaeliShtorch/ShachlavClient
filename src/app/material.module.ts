10f1e2a7b6f80d53b8017fa49d17a8e4052879c0
PninaM <45176849+PninaM@users.noreply.github.com>
2020-07-15 11:27:26 +0300

    ChavaTest

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule,MatToolbarModule,MatSidenavModule,MatListModule,MatIconModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material';
 import {MatInputModule} from '@angular/material';
 import {MatDatepickerModule} from '@angular/material';
@@ -7,8 +7,10 @@ import {MatNativeDateModule} from '@angular/material';
 
 
 @NgModule({
-    imports:[MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule],
-    exports:[MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule]
+    imports:[MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule,MatToolbarModule, MatSidenavModule,MatListModule,MatIconModule
+    ],
+    exports:[MatButtonModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule, MatToolbarModule,MatSidenavModule,MatListModule,MatIconModule
+    ]
 })
 export class MaterialModule{
 
