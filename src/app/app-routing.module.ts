d690f11cd7f4ef21932271edc4f1a2460b0bd15d
PninaM <45176849+PninaM@users.noreply.github.com>
2020-08-21 12:52:53 +0300

    pumpType

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,8 +16,9 @@ import { ProviderComponent } from './Components/provider/provider.component';
 import { DriverComponent } from './Components/driver/driver.component';
 import {OrderAddComponent } from './Components/order-add/order-add.component';
 import { DriverTaskComponent } from './Components/driver-task/driver-task.component';
-import { AddVeihcleComponent } from './Components/add-vehicle/add-vehicle.component';
+import {  AddVehicleComponent } from './Components/add-vehicle/add-vehicle.component';
 import { DriverWork } from './Models/driverWork.models';
+import { AddVehicleTypeComponent } from './Components/add-vehicle/add-PumpTypeVehicle.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home/subHome',  pathMatch: 'full' },
@@ -45,7 +46,8 @@ const routes: Routes = [
     { path: 'driverRegister',component: DriverRegisterComponent  },
     { path: 'providerRegister',component: ProviderRegisterComponent  },
     { path: 'customerRegister', component: CustomerRegisterComponent },
-    { path: 'addVehicle', component:AddVeihcleComponent},
+    { path: 'addVehicle', component:AddVehicleComponent},
+    { path: 'addPumpType', component:AddVehicleTypeComponent},
     { path: 'addDriverWork', component: DriverTaskComponent }
   
   ]  }
